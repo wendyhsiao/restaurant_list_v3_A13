02400a8c14a9d5f46cad298bc07012815e5e425f
wendy.hsiao <wendy19940331@gmail.com>
2019-09-08 01:34:20 +0800

    Add Seeder

---
+++ models/seeds/Seeder.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant.js')
+const restaurantList = require('../../restaurant.json').results
+
+mongoose.connect('mongodb://localhost/restaurantV1', { useNewUrlParser: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+
+  restaurantList.forEach(item => {
+    Restaurant.create({
+      name: item.name,
+      name_en: item.name_en,
+      category: item.category,
+      image: item.image,
+      location: item.location,
+      phone: item.phone,
+      google_map: item.google_map,
+      rating: item.rating,
+      description: item.description
+    })
+  })
+
+  console.log('done')
+})
