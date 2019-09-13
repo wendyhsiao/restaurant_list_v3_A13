3dd41727351f3eb8381b94083f55b9e3c5cb210a
wendy hsiao <wendy19940331@gmail.com>
2019-09-13 18:33:40 +0800

    Add user model

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Handlebars = require('handlebars')
+const User = require('../models/user.js')
 
 router.get('/login', (req, res) => {
   res.render('login')
